0d5804375db4f084dce97cf1e8b053d7dbf20671
Qi Guo <979918879@qq.com>
2023-09-13 11:24:49 +0800

    add custom plugin

---
+++ classic/src/remark/plugin-fields.mjs
@@ -0,0 +1,17 @@
+import { visit } from 'unist-util-visit';
+
+export const pluginFiled = () => {
+  const transformer = async (ast, vfile) => {
+    visit(ast, (node) => {
+      if (node.type === 'textDirective' ||
+        node.type === 'leafDirective' ||
+        node.type === 'containerDirective') {
+        console.log('node: ', node);
+        const isPluginField = node.name === 'plugin-field';
+        if (!isPluginField) return;
+      }
+    });
+  }
+
+  return transformer;
+};
